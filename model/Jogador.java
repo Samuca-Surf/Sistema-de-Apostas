bd587101ffebfecc4f786ae527a6aa0abdb47fd7
Samuca-Surf <samuca.lkm@gmail.com>
2025-09-24 18:14:34 -0300

    Add files via upload

---
+++ model/Jogador.java
@@ -0,0 +1,64 @@
+package model;
+
+import java.util.List;
+
+public class Jogador {
+    private String nome;
+    private String email;
+    private String senha;
+    private Double saldo;
+    private String id;
+    public Jogador() {}
+
+    public Jogador(String nome, Double saldo) {
+        this.nome = nome;
+        this.saldo = saldo;
+    }
+
+    public boolean apostar(Double valor){
+        if (valor <= saldo){
+            saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+
+    public void adicionarSaldo(Double valor){
+        saldo += valor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Double getAposta() {
+        return saldo;
+    }
+
+    public void setAposta(Double saldo) {
+        this.saldo = saldo;
+    }
+
+
+
+}
