0f7ba973b04d5fb40aee456c6a816f8200e4b692
Samuca-Surf <samuca.lkm@gmail.com>
2025-09-24 18:14:58 -0300

    Add files via upload

---
+++ src/model/Usuario.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Usuario {
+    private String id;
+    private String nome;
+    private String email;
+    private String senha;
+
+    public Usuario(String id, String nome, String email, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+}
